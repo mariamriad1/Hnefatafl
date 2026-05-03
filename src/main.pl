163409b794768b5ea9d58f5db84a853a69f64446
mennamohammed1234 <menna123momo@gmail.com>
2026-05-03 15:31:34 +0300

    Update main.pl

--- src/main.pl
+++ src/main.pl
@@ -1,28 +1,29 @@
 play :-
     initial_board(Board),
     write('Choose your side (attacker/defender): '),
-    read(HumanSide),
-    game_loop(Board, attacker, HumanSide).
+    read(Human_role),
+    game_loop(Board, attacker, Human_role).
 
-game_loop(Board, Curr_player, HumanSide) :-
+game_loop(Board, Curr_player, Human_role) :-
     print_board(Board),
     (defenders_win(Board) -> 
         write('Defenders Win!') ;
     attackers_win(Board) -> 
         write('Attackers Win!') ;
-    take_turn(Board, Curr_player, HumanSide, NewBoard),
+    take_turn(Board, Curr_player, Human_role, NewBoard),
     switch_player(Curr_player, Next_player),
-    game_loop(NewBoard, Next_player, HumanSide)).
+    game_loop(NewBoard, Next_player, Human_role)).
 
 switch_player(attacker, defender).
 switch_player(defender, attacker).
 
 take_turn(Board, Curr_player, Curr_player, NewBoard) :-
-    write(' please enter Your move (R1-C1-R2-C2): '),
+    write('Your move (R1-C1-R2-C2): '),
     read(R1-C1-R2-C2),
     move_piece(Board, R1, C1, R2, C2, TempBoard),
     nth0(R2, TempBoard, Row),
     nth0(C2, Row, Moved_Piece),
     capture_if_sandwiched(TempBoard, R2, C2, Moved_Piece, NewBoard).
 
-take_turn(Board, Curr_player, HumanSide, NewBoard) :- %ai_turn
+take_turn(Board, Curr_player, Human_role, NewBoard) :-
+    Curr_player \=Human_role, 
